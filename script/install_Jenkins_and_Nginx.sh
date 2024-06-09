0e658434199a4974a78303a9dd8e1e40243b9980
Dennis Owie <dennysben@gmail.com>
2024-06-09 11:18:41 +0200

    adding jenkins and nginx installation script

--- script/install_Jenkins_and_Nginx.sh
+++ script/install_Jenkins_and_Nginx.sh
@@ -2,11 +2,17 @@
 
 sudo apt update
 
+sudo apt install nginx
+
+echo "Nginx installed successfully. Prepararing for Jenkins installation next.."
+
+sudo apt update
+
 sudo apt install openjdk-11-jdk -y
 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-  
+
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null

022bcece0039b19e6335daaab3785803ba53be06
Dennis Owie <dennysben@gmail.com>
2024-06-12 08:28:46 +0200

    making correct reference to instance local name

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "jenkins_record" {
   name    = "${var.jenkins_domain}.${data.aws_route53_zone.hosted_zone.name}"
   type    = "A"
   ttl     = "300"
-  records = [aws_instance.jenkins-server.public_ip]
+  records = [aws_instance.server.public_ip]
 }
 
 resource "aws_route53_record" "gitlab_record" {
@@ -15,5 +15,5 @@ resource "aws_route53_record" "gitlab_record" {
   name    = "${var.gitlab_domain}.${data.aws_route53_zone.hosted_zone.name}"
   type    = "A"
   ttl     = "300"
-  records = [aws_instance.jenkins-server.public_ip]
+  records = [aws_instance.server.public_ip]
 }
